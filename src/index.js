d0cbaae97e3d133af36996205540dbed54136c33
m1javad <mohammadjavad13811381@gmail.com>
2023-08-13 14:39:52 +0330

    navbar and dropdown finished

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
+import LoadCategory from "../components/dropdownmenu";
+/*disable closing drop down on click */
+
 $(document).on('click', 'nav .dropdown-menu', function (e) {
     e.stopPropagation();
   });
   $(document).on('click', 'Dropdown3', function () {
     document.getElementsByClassName("dropdown-menu").stopPropagation();
   });
+
+
+LoadCategory();
